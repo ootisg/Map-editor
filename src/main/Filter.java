6b82e4d754cb4541ba9516aade7b32e50dabddb2
ootisg <ootisg@illcomics.com>
2021-05-18 17:51:02 -0500

    Small tweak to Filter

--- src/main/Filter.java
+++ src/main/Filter.java
@@ -7,25 +7,14 @@ import java.util.HashMap;
 
 public class Filter extends DisplayableImageElement {
 	
-	BufferedImage texture;
-	DisplayBox box = new DisplayBox (new Rectangle (0,0,1,1), getParent ());
-	OptionsMenu menu = new OptionsMenu (new Rectangle (200, 100, 140, 20), this.getParent().getMainPanel(),this);
+	private BufferedImage texture;
+	private DisplayBox box = new DisplayBox (new Rectangle (0,0,1,1), getParent ());
+	private OptionsMenu menu = new OptionsMenu (new Rectangle (200, 100, 140, 20), this.getParent().getMainPanel(),this);
 	
 	public Filter(GuiComponent parent) {
 		super(parent);
 		box.hide ();
 		menu.hide();
-		String [] options = new String [] {"option 1", "lol meme", "ha dumb idiot", "option 7"};
-		TileSelection check = new TileSelection (new Rectangle (200, 120, 140, 80), menu);
-		check.setRequest("select a region");
-		TileSelection check2 = new TileSelection (new Rectangle (200, 200, 140, 80), menu);
-		check2.setRequest("select a region");
-		menu.addContent (check);
-<<<<<<< HEAD
-		menu.addContent(check2);
-=======
-		
->>>>>>> 6e3b133421fdc9e83a4d05ffecff5a85e79c8e6d
 	}
 	
 	public void runFilterCode () {
@@ -52,6 +41,12 @@ public class Filter extends DisplayableImageElement {
 	}
 	public void dontShowName () {
 		box.hide();
+	}
+	protected DisplayBox getDisplayBox () {
+		return box;
+	}
+	protected OptionsMenu getOptionsMenu () {
+		return menu;
 	}
 	
 }
